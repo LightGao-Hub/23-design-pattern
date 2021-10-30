221250e0c09aa4ce300bd012beefa52efa92ceeb
gl <gaoliang@stargraph.cn>
2021-10-30 17:17:33 +0800

    [1.0]外观模型优化

--- src/main/java/org/example/design/structural/facade/NanShanFacade.java
+++ src/main/java/org/example/design/structural/facade/NanShanFacade.java
@@ -6,15 +6,11 @@ package org.example.design.structural.facade;
  */
 public class NanShanFacade implements Facade {
 
-    private final Industry industry = new NanShanIndustry();
-    private final Bank bank = new NanShanBank();
-    private final Taxes taxes = new NanShanTaxes();
-
     @Override
     public Company registerCompany(String companyName) {
-        String companyId = this.industry.register(companyName);
-        String bankAccount = this.bank.openAccount(companyId);
-        String taxCode = this.taxes.applyTaxCode(bankAccount);
+        String companyId = new NanShanIndustry().register(companyName);
+        String bankAccount = new NanShanBank().openAccount(companyId);
+        String taxCode = new NanShanTaxes().applyTaxCode(bankAccount);
         return new NanShanCompany(companyId, companyName, bankAccount, taxCode);
     }
 }
