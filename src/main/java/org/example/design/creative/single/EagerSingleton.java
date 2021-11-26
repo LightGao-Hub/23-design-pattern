f2a44228ef1b5e0d65009366c6b59384163c2ecf
gaoliang <gaoliang@stargraph.cn>
2021-11-26 14:32:31 +0800

    [1.0]优化单例模式解释

--- src/main/java/org/example/design/creative/single/EagerSingleton.java
+++ src/main/java/org/example/design/creative/single/EagerSingleton.java
@@ -1,7 +1,8 @@
 package org.example.design.creative.single;
 
 /**
- *  饿汉式单例
+ *  饿汉式单例：虽然是static修饰的成员变量，但并不保障一定是懒加载，因为此类加载的方式有很多，
+ *  假设此类有其他静态变量或函数被调用，那么就会加载到instance变量，从而变成了非懒加载单例
  *
  * Author: GL
  * Date: 2021-11-24
--- src/main/java/org/example/design/creative/single/StaticSingleton.java
+++ src/main/java/org/example/design/creative/single/StaticSingleton.java
@@ -13,7 +13,7 @@ public class StaticSingleton {
     }
 
     /**
-     * 类级的内部类，也就是静态的成员式内部类，该内部类的实例与外部类的实例
+     * private修饰避免被外部调用的内部类，也就是静态的成员式内部类，该内部类的实例与外部类的实例
      * 没有绑定关系，而且只有被调用到时才会装载，从而实现了延迟加载。
      */
     private static class SingletonHolder {
