9d63bd79e998a38eccf8c22eabec9d02057ff592
gaoliang <gaoliang@stargraph.cn>
2021-11-30 16:54:07 +0800

    [1.0]优化工厂模式

--- src/main/java/org/example/design/creative/factory/Explain.java
+++ src/main/java/org/example/design/creative/factory/Explain.java
@@ -1,9 +1,9 @@
 package org.example.design.creative.factory;
 
 /**
- *  常用的工厂模式分为两种：
+ *  常用的工厂方法模式分为两种：
  *      1、简单工厂模式[参考simple]
- *      2、标准工厂模式[参考complete]
+ *      2、标准工厂方法模式[参考complete]
  *      3、单例 + 工厂模式[参考perfect]
  *
  * Author: GL
